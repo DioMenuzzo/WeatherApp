ad1dfff4307ee51b4a426d813c9cc12b53bd62f8
Dio Menuzzo <118781259+DioMenuzzo@users.noreply.github.com>
2024-01-21 17:41:05 -0300

    main.py

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import requests
 
-def get_weather(city):
-    api_key = "0ff981148bc03fb4474e952c96822900"
-    base_url = "https://api.openweathermap.org/data/2.5/weather?"
-    url = base_url + "appid=" + api_key + "&q=" + city
-    
-    def kelvin_to_celsius_fahrenheit(kelvin):
-        celsius = kelvin - 273.15
-        fahrenheit = celsius * (9/5) + 32
-        return celsius, fahrenheit
-    
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        weather_data = response.json()
-    
-        description = weather_data["weather"][0]["description"]
-        temp = weather_data["main"]["temp"]
-        temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp)
-        feels_like = weather_data["main"]["feels_like"]
-        feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like)
-        humidity = weather_data["main"]["humidity"]
-        
-        print(f"Weather in {city.capitalize()}:")
-        print(f"Description: {description}")
-        print(f"Temperature: {temp_celsius:.2f} C°, {temp_fahrenheit:.2f} F°:")
-        print(f"Feels like: {feels_like_celsius:.2f} C°, {feels_like_fahrenheit:.2f} F°")
-        print(f"Humidity: {humidity}%")
-        
-    else:
+API_KEY = "0ff981148bc03fb4474e952c96822900"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+
+def kelvin_to_celsius_fahrenheit(kelvin: float) -> tuple[float, float]:
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
+
+def fetch_data_from_api(city: str) -> dict:
+    url = f'{BASE_URL}appid={API_KEY}&q={city}'
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+    except Exception as err:
         print("Failed to fetch weather information.")
-        
-def weather_forecast():
+    return {}
+
+
+def print_weather_info(city: str) -> None:
+    weather_data = fetch_data_from_api(city)
+
+    if not weather_data:
+        return
+
+    description = weather_data["weather"][0]["description"]
+    temp = weather_data["main"]["temp"]
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp)
+    feels_like = weather_data["main"]["feels_like"]
+    feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(
+        feels_like)
+    humidity = weather_data["main"]["humidity"]
+
+    print(f"Weather in {city.capitalize()}:")
+    print(f"Description: {description}")
+    print(f"Temperature: {temp_celsius:.2f} C째, {temp_fahrenheit:.2f} F째:")
+    print(
+        f"Feels like: {feels_like_celsius:.2f} C째, {feels_like_fahrenheit:.2f} F째")
+    print(f"Humidity: {humidity}%")
+
+
+if __name__ == '__main__':
     print("Weather Forecast")
     city = input("Enter a city name: ")
-    
-    get_weather(city)
-    
-weather_forecast()
+    print_weather_info(city)
